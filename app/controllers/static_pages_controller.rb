d4c32fb882e37d684688969581c7db00d4c851ec
maru0 <s15t265@stu.kagawa-u.ac.jp>
2018-05-21 09:12:18 +0000

    Finish layout and routesd

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,6 @@ class StaticPagesController < ApplicationController
   def root
   end
 
+  def contact
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def full_title(page_title)
+        base_title = "Ruby on Rails Tutorial Sample App"
+        title_home = "Home"
+        if page_title.empty?
+            "#{title_home} | #{base_title}"
+        else
+            "#{page_title} | #{base_title}"
+        end
+    end
 end
